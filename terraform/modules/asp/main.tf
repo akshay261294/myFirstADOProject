10c5e506916dd6e1cd1277c20f83d7fe1a09e88b
abharatd <akshay.bharatdwaj@capgemini.com>
2025-02-06 15:41:53 +0530

    update the available name for azure web app

--- terraform/modules/asp/main.tf
+++ terraform/modules/asp/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 }
 
 resource "azurerm_linux_web_app" "webapp" {
-  name                = "azure-webapp-test-akshay"
+  name                = "azure-webapp-akshay-test"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.appserviceplan.id
