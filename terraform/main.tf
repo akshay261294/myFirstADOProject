2618ee5fb11ed0046868622c2042860d14e9a835
abharatd <akshay.bharatdwaj@capgemini.com>
2025-02-06 15:33:40 +0530

    use environmrnt varibale to deloy terraform module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,3 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "azure-ResourceGroup"
-  location = "Central India"
-}
-
-resource "azurerm_service_plan" "appserviceplan" {
-  name                = "azure-webapp-asp"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "S1"
-}
-
-resource "azurerm_linux_web_app" "webapp" {
-  name                  = "azure-webapp-test-akshay"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  service_plan_id       = azurerm_service_plan.appserviceplan.id
-  depends_on            = [azurerm_service_plan.appserviceplan]
-  https_only            = true
-  site_config {
-    minimum_tls_version = "1.2"
-
-    application_stack {
-      java_version       = "11"            # Specify the Java version
-      java_server        = "TOMCAT"        # Specify the server (e.g., TOMCAT, JBoss, etc.)
-      java_server_version = "9.0"          # Specify the version of the Tomcat server (or other server)
-    }
-  }
-
-  app_settings = {
-    "JAVA_HOME"               = "/usr/lib/jvm/java-11-openjdk"
-    "MAVEN_HOME"              = "/usr/share/maven"
-    "MAVEN_OPTS"              = "-Xms512m -Xmx1024m"
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-  }
+module "app_service_plan" {
+  source = "./modules/asp"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,3 @@
 variable "location" {
   default = "East US"
 }
-
-variable "subscription_id" {
-  type = string
-}
