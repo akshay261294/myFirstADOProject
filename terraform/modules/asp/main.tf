1041d07b0cfac3a94951212a4b5450bc1a6c19fd
abharatd <akshay.bharatdwaj@capgemini.com>
2025-02-06 16:57:08 +0530

    update java server

--- terraform/modules/asp/main.tf
+++ terraform/modules/asp/main.tf
@@ -11,6 +11,32 @@ resource "azurerm_service_plan" "appserviceplan" {
   sku_name            = "S1"
 }
 
+# resource "azurerm_linux_web_app" "webapp" {
+#   name                = "azure-webapp-akshay-test"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   service_plan_id     = azurerm_service_plan.appserviceplan.id
+#   depends_on          = [azurerm_service_plan.appserviceplan]
+#   https_only          = true
+#   site_config {
+#     minimum_tls_version = "1.2"
+#
+#     application_stack {
+#       java_version        = "11"
+#       java_server         = "JAVA"
+#       java_server_version = "11.0"
+#     }
+#   }
+#
+#   app_settings = {
+#     "JAVA_HOME"                           = "/usr/lib/jvm/java-11-openjdk"
+#     "MAVEN_HOME"                          = "/usr/share/maven"
+#     "MAVEN_OPTS"                          = "-Xms512m -Xmx1024m"
+#     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+#   }
+# }
+
+
 resource "azurerm_linux_web_app" "webapp" {
   name                = "azure-webapp-akshay-test"
   location            = azurerm_resource_group.rg.location
@@ -18,20 +44,17 @@ resource "azurerm_linux_web_app" "webapp" {
   service_plan_id     = azurerm_service_plan.appserviceplan.id
   depends_on          = [azurerm_service_plan.appserviceplan]
   https_only          = true
+
   site_config {
     minimum_tls_version = "1.2"
-
     application_stack {
       java_version        = "11"
-      java_server         = "JAVA"
-      java_server_version = "11.0"
+      java_server         = "TOMCAT"
+      java_server_version = "9.0"
     }
   }
 
   app_settings = {
-    "JAVA_HOME"                           = "/usr/lib/jvm/java-11-openjdk"
-    "MAVEN_HOME"                          = "/usr/share/maven"
-    "MAVEN_OPTS"                          = "-Xms512m -Xmx1024m"
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
   }
 }
