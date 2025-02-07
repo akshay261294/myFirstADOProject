7934ce8faa85b3e4b9d38d3675a33b00b327c3aa
abharatd <akshay.bharatdwaj@capgemini.com>
2025-02-07 11:27:55 +0530

    use windo web app

--- terraform/modules/asp/main.tf
+++ terraform/modules/asp/main.tf
@@ -7,8 +7,8 @@ resource "azurerm_service_plan" "appserviceplan" {
   name                = "azure-webapp-asp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "S1"
+  os_type             = "Windows"
+  sku_name            = "B1"
 }
 
 # resource "azurerm_linux_web_app" "webapp" {
@@ -18,43 +18,47 @@ resource "azurerm_service_plan" "appserviceplan" {
 #   service_plan_id     = azurerm_service_plan.appserviceplan.id
 #   depends_on          = [azurerm_service_plan.appserviceplan]
 #   https_only          = true
+#
 #   site_config {
 #     minimum_tls_version = "1.2"
-#
 #     application_stack {
 #       java_version        = "11"
-#       java_server         = "JAVA"
-#       java_server_version = "11.0"
+#       java_server         = "TOMCAT"
+#       java_server_version = "9.0"
 #     }
 #   }
 #
 #   app_settings = {
-#     "JAVA_HOME"                           = "/usr/lib/jvm/java-11-openjdk"
-#     "MAVEN_HOME"                          = "/usr/share/maven"
-#     "MAVEN_OPTS"                          = "-Xms512m -Xmx1024m"
 #     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
 #   }
 # }
 
 
-resource "azurerm_linux_web_app" "webapp" {
-  name                = "azure-webapp-akshay-test"
+# resource "azurerm_resource_group" "example" {
+#   name     = "${var.prefix}-resources"
+#   location = var.location
+# }
+#
+# resource "azurerm_service_plan" "example" {
+#   name                = "${var.prefix}-sp"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   os_type             = "Windows"
+#   sku_name            = "B1"
+# }
+
+resource "azurerm_windows_web_app" "web-app" {
+  name                = "java-akshay-basic-example"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.appserviceplan.id
-  depends_on          = [azurerm_service_plan.appserviceplan]
-  https_only          = true
 
   site_config {
-    minimum_tls_version = "1.2"
     application_stack {
-      java_version        = "11"
-      java_server         = "TOMCAT"
-      java_server_version = "9.0"
+      current_stack          = "java"
+      java_version           = "11"
+      java_container         = "JAVA"
+      java_container_version = "SE"
     }
-  }
-
-  app_settings = {
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
   }
 }
