5954331e60cf406b357a88acdf1ac0c72905a473
abharatd <akshay.bharatdwaj@capgemini.com>
2025-02-07 11:47:45 +0530

    use windows web app

--- terraform/modules/asp/main.tf
+++ terraform/modules/asp/main.tf
@@ -11,42 +11,6 @@ resource "azurerm_service_plan" "appserviceplan" {
   sku_name            = "B1"
 }
 
-# resource "azurerm_linux_web_app" "webapp" {
-#   name                = "azure-webapp-akshay-test"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   service_plan_id     = azurerm_service_plan.appserviceplan.id
-#   depends_on          = [azurerm_service_plan.appserviceplan]
-#   https_only          = true
-#
-#   site_config {
-#     minimum_tls_version = "1.2"
-#     application_stack {
-#       java_version        = "11"
-#       java_server         = "TOMCAT"
-#       java_server_version = "9.0"
-#     }
-#   }
-#
-#   app_settings = {
-#     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-#   }
-# }
-
-
-# resource "azurerm_resource_group" "example" {
-#   name     = "${var.prefix}-resources"
-#   location = var.location
-# }
-#
-# resource "azurerm_service_plan" "example" {
-#   name                = "${var.prefix}-sp"
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-#   os_type             = "Windows"
-#   sku_name            = "B1"
-# }
-
 resource "azurerm_windows_web_app" "web-app" {
   name                = "java-akshay-basic-example"
   location            = azurerm_resource_group.rg.location
@@ -57,8 +21,6 @@ resource "azurerm_windows_web_app" "web-app" {
     application_stack {
       current_stack          = "java"
       java_version           = "11"
-      java_container         = "JAVA"
-      java_container_version = "SE"
     }
   }
 }
